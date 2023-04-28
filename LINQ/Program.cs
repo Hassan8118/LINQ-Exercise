27edb848d41217608839016e31e6c65854959c0b
hassan8118 <mujtabahassan1234567@gmail.com>
2023-04-28 11:16:35 -0400

    Done

--- LINQ/Program.cs
+++ LINQ/Program.cs
@@ -4,7 +4,16 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+           List<string> videoGames = new List<string>() { "Rocket League", "Call Of Duty", "Fifa"};
+            videoGames.Add("Fortnite");
+            videoGames.Add("GTA RP");
+
+            var sortedGames = videoGames.OrderByDescending(name => name.Length);
+
+            foreach (var games in sortedGames)
+            {
+                Console.WriteLine(games);
+            }
         }
     }
 }
